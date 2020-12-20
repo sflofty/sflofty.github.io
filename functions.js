0dd4a9f34cf9d0268baf423700329013a12373e8
sflofty <59518349+sflofty@users.noreply.github.com>
2020-12-20 01:51:41 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,225 @@
+function loadCountries() {
+	var request = new XMLHttpRequest();
+	request.open('GET', 'https://api.covid19api.com/summary');
+
+	request.onload = function () {
+		var data = JSON.parse(this.response).Countries;
+
+		var sorted = [];
+		for(var i = 0; i < data.length; i++) {
+		    var country = data[i];
+		    countryToSlug[country.Country] = country.Slug;
+		    sorted.push(country.Country);
+		}
+
+		sorted.sort();
+		var dataList = document.getElementById('filterOptions');
+		sorted.forEach(function(item, index){
+			var option = document.createElement('option');
+			option.id = item;
+			option.value = item;
+		    dataList.appendChild(option);
+		});
+
+		updateFilter();
+	}
+	request.send();
+};
+
+function updateFilter(){
+	document.getElementById('countryFilter').innerHTML = '';
+
+	filter_countries.sort();
+
+	filter_countries.forEach(function(item, index){
+		//download data
+		if(!(item in countryData)) downloadCountry(item);
+
+		//create filter indicator
+		var div = document.createElement("div");
+
+		div.id = item;
+		div.className = 'country';
+		div.innerHTML = item + " ×";
+		div.addEventListener("click", function(){
+			filter_countries.splice(index, 1);
+			updateFilter();
+		});
+
+		document.getElementById('countryFilter').appendChild(div);
+	})
+
+	loadSummary();
+}
+
+function addFilter(){
+	var input = document.getElementById('filterInput').value;
+	document.getElementById('filterInput').value = '';
+
+	if(!(input in countryToSlug)) return;
+	if(filter_countries.includes(input)) return;
+
+	filter_countries.push(input); 
+	updateFilter();
+}
+
+function downloadCountry(country){
+	var url = "https://api.covid19api.com/total/country/" + countryToSlug[country];
+
+	var request = new XMLHttpRequest();
+	request.open('GET', url);
+
+	var preConfirmed = 0;
+	var preDeaths = 0;
+	var preRecovered = 0;
+
+	request.onload = function () {
+		var data = JSON.parse(this.response);
+
+		countryData[country] = {};
+		countryData[country]["history"] = {};
+
+		data.forEach(function (item, index){
+			item["New Confirmed"] = item.Confirmed - preConfirmed;
+			item["New Deaths"] = item.Deaths - preDeaths;
+			item["New Recovered"] = item.Recovered - preRecovered;
+
+			preConfirmed = item.Confirmed;
+			preDeaths = item.Deaths;
+			preRecovered = item.Recovered;
+
+			var date = item.Date.slice(0,10);
+			var date7DaysAgo = new Date(new Date(item.Date) - (7 * 24 * 60 * 60 * 1000)).toISOString().slice(0,10);
+
+			if(index > 6) item["± 7d Cases"] = item["New Confirmed"] - countryData[country]["history"][date7DaysAgo]["New Confirmed"];
+
+			delete item["Lat"];
+			delete item["Lon"];
+			delete item["City"];
+			delete item["CityCode"];
+			delete item["CountryCode"];
+			delete item["Province"];
+			delete item["Country"];
+			delete item["Date"];
+
+			countryData[country]["history"][date] = item;
+
+			if(index + 1 == data.length) countryData[country]["summary"] = item;
+		});
+
+		loadSummary();
+	}
+
+	console.log(countryData)
+	request.send();
+}
+
+function loadHeader(){
+	var header = document.getElementById("header");
+
+	//insert header from filter
+	filter_fields.forEach(function(item){
+		var cell = document.createElement("th");
+		cell.className = "number";
+		cell.innerHTML = item;
+		header.appendChild(cell);
+	});
+			
+}
+
+function loadSummary() {
+	var table = document.getElementById("summary_content");
+	table.innerHTML = '';
+
+	Object.keys(countryData).forEach(function(key) {
+		if (!filter_countries.includes(key)) return;
+
+		//insert row for key = country
+		var row = table.insertRow(-1);
+		row.innerHTML = key;
+		row.id = key;
+		row.addEventListener("click", function(){ showDetails(this.id) });
+
+		//insert columns from filter
+		filter_fields.forEach(function(item){
+			var column = row.insertCell(-1);
+			column.className = "number";
+			column.innerHTML = countryData[key]["summary"][item];
+		});
+	})
+}
+
+function showDetails(country){
+	//load14Days(country);
+/*
+	var activeCasesDiv = document.getElementById('activeCasesDiv');
+	activeCasesDiv.innerHTML = '';
+	var activeCasesData = getDailyCases(country);
+	var minX = activeCasesData[0].t;
+	var maxX = activeCasesData[activeCasesData.length-1].t;
+
+	var canvas = document.createElement("canvas");
+	var myChart = new Chart(canvas, {
+	  type: 'line',
+	  data: {
+	    datasets: [{
+	      label: 'Active',
+	      borderColor: '#1d90c2',
+	      borderWidth: 2,
+	      pointRadius: 0,
+	      fill: 'false',
+	      data: activeCasesData
+	    }]
+	  },
+	  options: {
+	  	responsive: true,
+		plugins: {
+			zoom: {
+				pan: {
+					enabled: true,
+					mode: 'x',
+					rangeMin: {
+						x: minX
+					},
+					rangeMax: {
+						x: maxX
+					},
+				},
+				zoom: {
+					enabled: true,
+					mode: 'x',
+					rangeMin: {
+						x: minX
+					},
+					rangeMax: {
+						x: maxX
+					},
+				}
+			}
+		},
+	    scales: {
+			yAxes: [{
+	            ticks: {
+	                beginAtZero: true
+	            }
+			}],
+			xAxes: [{
+				type: 'time',
+                time: {
+                    displayFormats: {
+                        month: 'YY-MM'
+                    }
+                }
+			}]
+		}
+	  }
+	});
+
+	activeCasesDiv.appendChild(canvas);
+*/
+  	document.getElementById("details").style.width = "100%";
+}
+
+function hideDetails() {
+	document.getElementById("details").style.width = "0%";
+}
