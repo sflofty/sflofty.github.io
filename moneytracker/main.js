b99fb5e92515e95f03b1b365577cacd936a810fb
sflofty <59518349+sflofty@users.noreply.github.com>
2022-04-17 14:29:59 +0200

    install service worker

--- moneytracker/main.js
+++ moneytracker/main.js
@@ -1,9 +1,23 @@
 balance = 1234.51;
-rate = 0;
-decimals = 5;
+rate = 0.0001;
+decimals = 4;
+
+if ('serviceWorker' in navigator){
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('sw.js')
+            .then(reg => {
+                console.log('service worker registered');
+            }).catch(err => {
+                console.log('service worker failed');
+            });
+    });
+}
+
+setInterval(displayNumber, 50);
 
 function displayNumber() {
-    balance += Math.random() + Math.floor(Math.random() * 5);
+    //balance += Math.random() + Math.floor(Math.random() * 5);
+    balance += rate;
 
     // reset number display
     var div = document.getElementById("numberDisplay");
@@ -12,8 +26,6 @@ function displayNumber() {
     // add digits in front of decimal
     var digits = balance.toFixed(decimals).toString().split('');
     digits.forEach(digit => addDigitSpan(div, digit));
-
-    var digitsBeforeDecimalPoint = balance.toString().length;
 }
 
 function addDigitSpan(div, digit){
