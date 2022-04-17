8aae209c6ed30de898943dc29482ee63afed388e
sflofty <59518349+sflofty@users.noreply.github.com>
2022-04-17 15:34:30 +0200

    notification

--- moneytracker/main.js
+++ moneytracker/main.js
@@ -1,6 +1,7 @@
 balance = 1234.51;
-rate = 0.0001;
-decimals = 4;
+rate = 0.001;
+decimals = 3;
+notificationLimit = balance + rate * 300;
 
 if ('serviceWorker' in navigator){
     window.addEventListener('load', () => {
@@ -18,6 +19,10 @@ setInterval(displayNumber, 50);
 function displayNumber() {
     //balance += Math.random() + Math.floor(Math.random() * 5);
     balance += rate;
+    if (balance > notificationLimit) {
+        notificationLimit = balance + rate * 300;
+        displayNotification('limit reached');
+    }
 
     // reset number display
     var div = document.getElementById("numberDisplay");
@@ -33,4 +38,29 @@ function addDigitSpan(div, digit){
     digitSpan.className = "digit";
     digitSpan.innerHTML = digit;
     div.append(digitSpan);
+}
+
+function displayNotification(body){
+    console.log(Notification.permission);
+    if (!("Notification" in window)) {
+        alert("This browser does not support desktop notification");
+    }else if (Notification.permission == 'granted'){
+        navigator.serviceWorker.getRegistrations()
+            .then(reg => {
+                const options = {
+                    body: body,
+                    vibrate: [100, 50, 100]
+                }
+                reg.showNotification('Hello', options);
+            });
+    }else if (Notification.permission !== "denied") {
+        Notification.requestPermission().then(function (permission) {
+          if (permission === "granted") {
+            var notification = new Notification(body);
+          }else if(permission == "denied") {
+              alert('permission denied');
+          }
+        });
+      }
+
 }
