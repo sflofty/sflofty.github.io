cf3116f654e60237904c486e886e251c21a2e565
sflofty <59518349+sflofty@users.noreply.github.co>
2022-04-19 00:10:57 +0200

    basic modal added

--- moneytracker/main.js
+++ moneytracker/main.js
@@ -5,6 +5,8 @@ notificationLimit = balance + rate * 300;
 
 setInterval(displayNumber, 50);
 
+//TODO: don't create new div, instead change content of old divs
+
 function displayNumber() {
     //balance += Math.random() + Math.floor(Math.random() * 5);
     balance += rate;
@@ -26,4 +28,34 @@ function addDigitSpan(div, digit){
     digitSpan.className = "digit";
     digitSpan.innerHTML = digit;
     div.append(digitSpan);
+}
+
+//MODAL
+var modal = document.getElementById("modalElement");
+var closeBtn = document.getElementById("close");
+
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
+
+function addListing(){
+    modal.style.display = "block";
+    addRow(Math.random());
+}
+
+function addRow(name){
+    var table = document.getElementById('cashFlowList');
+    var row = table.insertRow(0);
+
+    var nameCell = row.insertCell(0);
+    var buttonCell = row.insertCell(1);
+
+    nameCell.innerHTML = name;
+    buttonCell.innerHTML = "<Button />";
 }
